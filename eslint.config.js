9a06a4646b8b8368c2088ff1ef10580ff0cfb63c
moluoxixi <1983531544@qq.com>
2025-11-28 13:52:43 +0800

    feat: eslint修改

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
 import eslintConfig from '@moluoxixi/eslintconfig'
 
 export default eslintConfig({
-  ignores: [
-    'src/components/ConfigForm/**',
-    '.husky/**',
-    '**/*.md',
-  ],
+  ignores: [],
 })
